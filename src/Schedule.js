b5216ee154967b90f3a078e8ce78ead2cbee1f30
Jiawen Wu <jiawenwu@berkeley.edu>
2023-11-22 19:38:23 -0800

    'total hours required' will change accordingly when task is checked

--- src/Schedule.js
+++ src/Schedule.js
@@ -1,46 +1,63 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Box, Typography, Paper, Checkbox } from '@mui/material';
 
 const Schedule = ({ tasks }) => {
-  const totalHours = tasks.reduce((total, task) => total + task.duration, 0);
-  const dailyHours = Math.max(totalHours / tasks.length, 10);
-  const weekdays = {
+  const [weekdays, setWeekdays] = useState({
     Monday: { title: "Monday", totalHours: 0, tasks: [] },
     Tuesday: { title: "Tuesday", totalHours: 0, tasks: [] },
     Wednesday: { title: "Wednesday", totalHours: 0, tasks: [] },
     Thursday: { title: "Thursday", totalHours: 0, tasks: [] },
     Friday: { title: "Friday", totalHours: 0, tasks: [] },
-  };
+  });
 
-  const handleTaskCheck = (weekday, isChecked, duration) => {
-    if (isChecked) {
-      duration = -duration;
+  const totalHours = tasks.reduce((total, task) => total + task.duration, 0);
+
+  useEffect(() => {
+    // This effect will run whenever tasks or weekdays change
+    const updatedWeekdays = { ...weekdays };
+
+    for (const weekday of Object.keys(updatedWeekdays)) {
+      updatedWeekdays[weekday].totalHours = 0;
+      updatedWeekdays[weekday].tasks = [];
     }
-    weekdays[weekday].totalHours += duration;
-  }
-  
-  
-  const binToDay = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
-  for (const task of tasks) {
-    for (const weekday of binToDay) {
-      const data = weekdays[weekday];
-      if (task.duration + data.totalHours <= dailyHours) {
-        weekdays[weekday].totalHours += task.duration;
-        weekdays[weekday].tasks.push(task);
-        console.log(task.duration);
-        break;
+
+    const dailyHours = Math.max(totalHours / tasks.length, 10);
+
+    for (const task of tasks) {
+      for (const weekday of Object.keys(updatedWeekdays)) {
+        const data = updatedWeekdays[weekday];
+        if (task.duration + data.totalHours <= dailyHours) {
+          updatedWeekdays[weekday].totalHours += task.duration;
+          updatedWeekdays[weekday].tasks.push(task);
+          break;
+        }
       }
     }
-  }
-  
-  
+
+    setWeekdays(updatedWeekdays);
+  }, [tasks, totalHours]);
+
+  const handleTaskCheck = (weekday, taskIndex, isChecked, duration) => {
+    setWeekdays((prevWeekdays) => {
+      const updatedWeekdays = { ...prevWeekdays };
+
+      if (isChecked) {
+        duration = -duration;
+      }
+
+      updatedWeekdays[weekday].totalHours += duration;
+
+      return updatedWeekdays;
+    });
+  };
+
   return (
     <Box>
       <Typography variant="h6" component="h1" color="text.primary" textAlign="center">
         Here is the schedule
       </Typography>
       <Paper>
-        {binToDay.map((weekday, index) => (
+        {Object.keys(weekdays).map((weekday, index) => (
           <div key={index}>
             <Typography variant="h6" component="h2">
               {weekdays[weekday].title}
@@ -51,13 +68,11 @@ const Schedule = ({ tasks }) => {
             {weekdays[weekday].tasks.map((task, taskIndex) => (
               <div key={taskIndex} style={{ display: 'flex', alignItems: 'center' }}>
                 <Checkbox
-                  onChange={(event) => handleTaskCheck(weekday, taskIndex, event.target.checked, task.duration)
-                  }
+                  onChange={(event) => handleTaskCheck(weekday, taskIndex, event.target.checked, task.duration)}
                 />
                 <Typography variant="body1" component="p" style={{ marginLeft: '8px' }}>
                   {`Task ${taskIndex + 1}: ${task.name}, Duration: ${task.duration}`}
                 </Typography>
-
               </div>
             ))}
           </div>
@@ -66,6 +81,5 @@ const Schedule = ({ tasks }) => {
     </Box>
   );
 };
-
 
 export default Schedule;
